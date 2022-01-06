49e7b94a3ceb3d8cd51ad0f0c8aaf4baab31a90e
Chuck Lemons <chuck.lemons@q2.com>
2022-01-06 17:13:46 -0600

    Add code for adding blocks to blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,63 +1,93 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
         self.current_transactions = []
-        
-    def new_block(self):
+        self.chain = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
-        pass
-
-    def new_transaction(self, sender, recipient, amount):
         """
-        Creates a new transaction to go into the next mined Block
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction
+        Create a new Block in the Blockchain
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
         """
 
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
 
-        # return self.last_block['index'] + 1
-    
-    @staticmethod
-    def hash(block):
-        # Hashes a Block
-        pass
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
 
-    def proof_of_work(self, last_proof):
-        """
-        Simple Proof of Work Algorithm:
-         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
-         - p is the previous proof, and p' is the new proof
-        :param last_proof: <int>
-        :return: <int>
-        """
+    # def new_transaction(self, sender, recipient, amount):
+    #     """
+    #     Creates a new transaction to go into the next mined Block
+    #     :param sender: <str> Address of the Sender
+    #     :param recipient: <str> Address of the Recipient
+    #     :param amount: <int> Amount
+    #     :return: <int> The index of the Block that will hold this transaction
+    #     """
 
-        proof = 0
-        while self.valid_proof(last_proof, proof) is False:
-            proof += 1
+    #     self.current_transactions.append({
+    #         'sender': sender,
+    #         'recipient': recipient,
+    #         'amount': amount,
+    #     })
 
-        return proof
+    #     # return self.last_block['index'] + 1
+    
+    # @staticmethod
+    # def hash(block):
+    #     # Hashes a Block
+    #     pass
 
-    @staticmethod
-    def valid_proof(last_proof, proof):
-        """
-        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :return: <bool> True if correct, False if not.
-        """
+    # @property
+    # def last_block(self):
+    #     # Returns the last Block in the chain
+    #     pass
 
-        guess = f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
+    # def proof_of_work(self, last_proof):
+    #     """
+    #     Simple Proof of Work Algorithm:
+    #      - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+    #      - p is the previous proof, and p' is the new proof
+    #     :param last_proof: <int>
+    #     :return: <int>
+    #     """
+
+    #     proof = 0
+    #     while self.valid_proof(last_proof, proof) is False:
+    #         proof += 1
+
+    #     return proof
+
+    # @staticmethod
+    # def valid_proof(last_proof, proof):
+    #     """
+    #     Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+    #     :param last_proof: <int> Previous Proof
+    #     :param proof: <int> Current Proof
+    #     :return: <bool> True if correct, False if not.
+    #     """
+
+    #     guess = f'{last_proof}{proof}'.encode()
+    #     guess_hash = hashlib.sha256(guess).hexdigest()
+    #     return guess_hash[:4] == "0000"
--- test_blockchain.py
+++ test_blockchain.py
@@ -5,15 +5,21 @@ class TestBlockchain(unittest.TestCase):
 
     def test_initial_blockchain(self):
         blockchain = Blockchain()
+        print (blockchain.last_block)
 
-        assert(len(blockchain.chain) == 0)
+        assert(len(blockchain.chain) == 1)
         assert(len(blockchain.current_transactions) == 0)
-    
-    def test_adding_transaction_to_blockchain(self):
-        blockchain = Blockchain()
+        
+        last_block = blockchain.last_block
+        assert(last_block['index'] == 1)
+        assert(last_block['proof'] == 100)
+        assert(last_block['previous_hash'] == 1)
 
-        rc = blockchain.new_transaction('bogus-sender', 'bogus-recipient', 100)
-        assert(len(blockchain.current_transactions) == 1)
+    # def test_adding_transaction_to_blockchain(self):
+    #     blockchain = Blockchain()
+
+    #     rc = blockchain.new_transaction('bogus-sender', 'bogus-recipient', 100)
+    #     assert(len(blockchain.current_transactions) == 1)
 
 if __name__ == '__main__':
     unittest.main()
